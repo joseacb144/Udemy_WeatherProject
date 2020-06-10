a00f22ca164d49122a6c65bf847d0f0880a8839d
Jose Chavez <joseacb144@gmail.com>
2020-06-10 05:47:41 -0500

    Update app.js to send weather description, temp, and weather icon.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const https = require("https");
 // Initialize a new Express app
 const app = express();
 
-
 app.get("/",function(req, res){
 // The link is long so I assigned it to constant url
 const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=86adea560ce2c6405450b6a93f97e7d9&units=metric";
@@ -13,17 +12,28 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=86ad
   https.get(url, function(response){
     console.log(response.statusCode);
 
+    // Get ahold of the data from response
     response.on("data", function(data){
+      // Parse JSON data
       const weatherData = JSON.parse(data);
+      // Assign temperature to temp
       const temp = weatherData.main.temp;
+      // Assign weather description to weatherDescription
       const weatherDescription = weatherData.weather[0].description;
-      console.log(temp);
-      console.log(weatherDescription);
+
+      const icon = weatherData.weather[0].icon;
+      const iconURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("<p>The weather is currently " + weatherDescription + "<p>");
+      res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+      res.write("<img src=" + iconURL +">");
+
+      res.send();
+// HUGE NOTE: You can only have 1 res.send or you will get an error.
+// NOTE: You can have more than 1 res.write.
     })
   })
-  res.send("Server is up and running!");
 })
-
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
