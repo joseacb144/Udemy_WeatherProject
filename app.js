b8b67a6eca8fcac9f0384a88be0d50add9519ad1
Jose Chavez <joseacb144@gmail.com>
2020-06-10 05:19:35 -0500

    Test API connection by logging current temp on terminal.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-// Require Express module
+// Require Express module and https module
 const express = require("express");
+const https = require("https");
+
 // Initialize a new Express app
 const app = express();
 
 
 app.get("/",function(req, res){
+// The link is long so I assigned it to constant url
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=86adea560ce2c6405450b6a93f97e7d9&units=metric";
+  // Get the weather API data
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      console.log(temp);
+    })
+  })
   res.send("Server is up and running!");
 })
 
